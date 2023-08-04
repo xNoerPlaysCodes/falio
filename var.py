739c4be360c97ff648dbcd4d027a23334d2b7c13
xNoerPlaysCodes <97339279+xNoerPlaysCodes@users.noreply.github.com>
2023-08-04 14:40:20 +0530

    Update var.py NFR 1
    NFR1

--- var.py
+++ var.py
@@ -1,1 +1,6 @@
-tokenBot = "BOT TOKEN HERE"
+import discord
+# enter bot token here:
+tokenBot = "bot token here"
+intents = discord.Intents.default()
+intents.message_content = True  # Allows the bot to receive message (Message updates, message send, delete etc (required to function))
+PREFIX = ";" # The bot will respond to for example if you have prefix ; then it will respond to ;embedlol or ;help etc
