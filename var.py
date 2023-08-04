4842f88716c25952e23eac005b32d1bb07d539aa
xNoerPlaysCodes <97339279+xNoerPlaysCodes@users.noreply.github.com>
2023-08-04 15:07:21 +0530

    mroe stuff

--- var.py
+++ var.py
@@ -1,6 +1,8 @@
 import discord
 # enter bot token here:
-tokenBot = "bot token here"
+tokenBot = "MTEzNjY3MDEyODExNzkxOTc1NA.GfXg7h._Ds7M7LsPNLg8WvfoNsBD2i4b7QKbegY0oUQHU"
 intents = discord.Intents.default()
-intents.message_content = True  # Allows the bot to receive message (Message updates, message send, delete etc (required to function))
-PREFIX = ";" # The bot will respond to for example if you have prefix ; then it will respond to ;embedlol or ;help etc
+intents.message_content = True  # Allows the bot to receive message events
+PREFIX = ";"
+ENABLE_SLASH_COMMANDS = True # SLASH COMMANDS. Set to False capital F to turn off.
+# ^^^^^^^^^^^^^^^ DOESNT WORK
