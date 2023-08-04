a3a8684db3df1592d3908a74da906e0561e96c69
xNoerPlaysCodes <97339279+xNoerPlaysCodes@users.noreply.github.com>
2023-08-04 14:38:26 +0530

    Create commands.py NFR 1
    NFR 1

---
+++ commands.py
@@ -0,0 +1,21 @@
+import discord
+
+def get_commands(client):
+    commands = {
+        "hello": f"Hello there, I'm {client.user}",
+        "ping": "pong",
+        "embedlol": discord.Embed(
+            title="A discord embed.",
+            description="A discord description by noerlol",
+            color=discord.Color(int("AF27E4", 16)),
+        ),
+        "help": discord.Embed(
+            title="Help Menu",
+            description="Hey, I'm just a work in progress so don't expect me to work!",
+            color=discord.Color(int("AF27E4", 16))
+        ),
+    }
+    # Set the footer using the set_footer method
+    commands["embedlol"].set_footer(text="ma foot")
+    commands["help"].set_footer(text="Bot by noerlol#0000")
+    return commands
