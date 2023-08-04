b5ec61c3be7d055686154383838ce855dbbdd7bd
xNoerPlaysCodes <97339279+xNoerPlaysCodes@users.noreply.github.com>
2023-08-04 14:37:09 +0530

    UPDATE - NFR-1
    NotForRelease (NFR) V1

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 import discord
-from var import tokenBot as token
+from var import tokenBot as TOKEN
+from var import PREFIX
+from commands import get_commands
 
-print("Logging in....")
-
+# Create a Discord client (bot)
 intents = discord.Intents.default()
-intents.typing = True
-intents.presences = False
-
+intents.message_content = True  # Allows the bot to receive message events
 client = discord.Client(intents=intents)
 
+# Event: When the bot is ready and connected to Discord
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user.name}')
+    print(f"We have logged in as {client.user}")
 
+# Event: When a message is received in a server the bot is a part of
 @client.event
 async def on_message(message):
-    # Ignore messages sent by the bot itself
+    # Ignore messages from the bot itself to avoid an infinite loop
     if message.author == client.user:
         return
 
-    print(f'Message received: {message.content}')  # Add this line to check the message content
+    # Split the message into the command and arguments
+    parts = message.content.split(" ", 1)
+    command = parts[0].lower()
+    arguments = parts[1] if len(parts) > 1 else ""
 
-    if message.content.startswith(';test'):
-        await message.channel.send(f'Hello, {message.author.name}.')
+    # Get the commands dictionary using the get_commands function from commands.py
+    commands = get_commands(client)
 
-    if message.content.startswith(';ping'):
-        # Calculate the bot's ping in milliseconds.
-        ping_ms = round(client.latency * 1000)
-        await message.channel.send(f'Ping is {ping_ms}ms')
+    # Check if the command is recognized
+    if command.startswith(PREFIX) and command[len(PREFIX):] in commands:
+        # Get the response from the commands dictionary
+        response = commands[command[len(PREFIX):]]
+        # If it's an embed, send it as an embed
+        if isinstance(response, discord.Embed):
+            await message.channel.send(embed=response)
+        else:
+            # Otherwise, send it as a normal message
+            await message.channel.send(response)
 
-    if message.content.startswith('mbgtk suc'):
-        await message.channel.send(f'I will send you to Jesus.')
-
-client.run(token)
+# Run the bot with the provided token
+client.run(TOKEN)
