89d41b963082858f3ef39cfb5a7aff98a0fc78c2
xNoerPlays <97339279+xNoerPlaysCodes@users.noreply.github.com>
2023-08-05 11:22:32 +0530

    Update var.py

--- var.py
+++ var.py
@@ -1,12 +1,10 @@
 import discord
-with open("config.txt") as config:
-    for line in config:
-        if line.startswith("token"):
-            token = line.strip().split('=', 1)
-            tokenBot = line.strip().strip("'")
-            break
+# enter bot token here:
+tokenBot = "BOT_TOKEN_HERE"
 intents = discord.Intents.default()
 intents.message_content = True  # Allows the bot to receive message events
 PREFIX = ";"
 ENABLE_SLASH_COMMANDS = True # SLASH COMMANDS. Set to False capital F to turn off.
 # ^^^^^^^^^^^^^^^ DOESNT WORK
+footer_text = "Bot by noerlol#0000"
+# Anywhere in commands.py with commands["COMMAND_NAME_HERE"].set_footer(text=footer) the text=footer will use this.
