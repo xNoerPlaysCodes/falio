2ca2d00bf936d576605b50ff51b4e764a6b20c05
xNoerPlays <97339279+xNoerPlaysCodes@users.noreply.github.com>
2023-08-06 12:45:18 +0530

    Update var.py

--- var.py
+++ var.py
@@ -6,5 +6,6 @@ intents.message_content = True  # Allows the bot to receive message events
 PREFIX = ";"
 ENABLE_SLASH_COMMANDS = True # SLASH COMMANDS. Set to False capital F to turn off.
 # ^^^^^^^^^^^^^^^ DOESNT WORK
-footer_text = "Bot by noerlol#0000"
-# Anywhere in commands.py with commands["COMMAND_NAME_HERE"].set_footer(text=footer) the text=footer will use this.
+footer_text = "Bot by noerlol#0000" # Credits, if you want you can change, changes the footer in most commands.
+owner = "noerlol"
+# The bot owner username if they have no tag at the end of name do only the name no #0000 for example not noerlol#0000 but noerlol or anything and if they have a tag do this UserName#1234 and replace with correct
