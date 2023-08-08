ce18a241d62e0bff47f49420300767accedb5c0d
xNoerPlays <97339279+xNoerPlaysCodes@users.noreply.github.com>
2023-08-08 13:20:50 +0530

    Moved commands.py to main.py

--- main.py
+++ main.py
@@ -14,18 +14,54 @@
 import discord
 import sys
 import requests
+import random
+import os
+import json
 from var import tokenBot as TOKEN
 from var import PREFIX
-from commands import get_commands
 from var import owner
 from var import api_key
-#define stopBot(message)
+from var import footer_text as footer
+from var import PlayingGame
+
+if api_key == "API_KEY_HERE":
+    meme_enabled = "false"
+else:
+    meme_enabled = "true"
+
+helpMenu = f"""
+            {PREFIX}hello
+            {PREFIX}ping
+            {PREFIX}help (This!)
+            {PREFIX}random
+            {PREFIX}meme (Is meme enabled? -> {meme_enabled})
+            {PREFIX}settag (Tag)
+            ^^^^^^^^^^^^^^^^^^^^ Sets Gifs what will be about to (Tag)
+            {PREFIX}ticket-add
+            Adds a ticket.
+            {PREFIX}ticket-rm
+            Removes your ticket.
+            """
+
+# DEFINTIONS OF CUST_FUNC
+def loadHasTicket():
+    filename = "user_tickets.json"
+    try:
+        with open(filename, 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {"users_with_tickets": {}}
+def saveHasTicket(data):
+    filename = "user_tickets.json"
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
+
 def stopBot(message):
     sys.exit(0)
 
 def isBot(user):
     return user.bot
-
+#########################
 tag = "random funny"
 
 # Create a Discord client (bot)
@@ -33,25 +69,26 @@ intents = discord.Intents.default()
 intents.message_content = True  # Allows the bot to receive message events
 client = discord.Client(intents=intents)
 
-# GIF FETCH THINGY
-async def fetch_gif_with_tag(tag):
-    url = f'https://api.giphy.com/v1/gifs/random?api_key={api_key}&tag={tag}'
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        data = response.json()
-        if 'data' in data and 'images' in data['data'] and 'original' in data['data']['images']:
-            gif_url = data['data']['images']['original']['url']
-            return gif_url
-        else:
-            print("Error parsing API response: 'image_original_url' key not found.")
-            return None
-    except requests.exceptions.RequestException as e:
-        print("Error while fetching GIF from Giphy API:", e)
-        return None
-    except KeyError as e:
-        print("Error parsing API response:", e)
-        return None
+#############################################################################################################
+# GIF FETCH THINGY                                                                                          #
+async def fetch_gif_with_tag(tag):                                                                          #
+    url = f'https://api.giphy.com/v1/gifs/random?api_key={api_key}&tag={tag}'                               #
+    try:                                                                                                    #
+        response = requests.get(url)                                                                        #
+        response.raise_for_status()  # Raise an exception for HTTP errors                                   #
+        data = response.json()                                                                              #
+        if 'data' in data and 'images' in data['data'] and 'original' in data['data']['images']:            #
+            gif_url = data['data']['images']['original']['url']                                             #
+            return gif_url                                                                                  #
+        else:                                                                                               #
+            print("Error parsing API response: 'image_original_url' key not found.")                        #
+            return None                                                                                     #
+    except requests.exceptions.RequestException as e:                                                       #
+        print("Error while fetching GIF from Giphy API:", e)                                                #
+        return None                                                                                         #
+    except KeyError as e:                                                                                   #
+        print("Error parsing API response:", e)                                                             #
+        return None##########################################################################################
 # Event: When the bot is ready and connected to Discord
 @client.event
 async def on_ready():
@@ -59,7 +96,7 @@ async def on_ready():
         print(f"Successful login as {client.user} with prefix {PREFIX} and no Giphy API KEY.")
     else:
         print(f"Successful login as {client.user} with prefix {PREFIX}\nand Giphy API KEY {api_key}")
-    await client.change_presence(activity=discord.Game(name=f'{PREFIX}help'))
+    await client.change_presence(activity=discord.Game(name=PlayingGame))
 
 # Event: When a message is received in a server the bot is a part of
 @client.event
@@ -91,27 +128,66 @@ async def on_message(message):
             await message.channel.send(gif_url)
         else:
             await message.channel.send("Oops, something went wrong while fetching the GIF.")
+    elif message.content == f"{PREFIX}help":
+        embed = discord.Embed(
+            title="Help Menu",
+            description=helpMenu,
+            color=discord.Color(int("AF27E4", 16)),
+        )
+        embed.set_footer(text=footer),
+        await message.channel.send(embed=embed)
+    elif message.content == f"{PREFIX}hello":
+        await message.channel.send(f"Hello there, I'm {client.user}!")
+    elif message.content == f"{PREFIX}ping":
+        await message.channel.send(f"Pong! {round(client.latency * 1000)}ms")
+    elif message.content == f"{PREFIX}random":
+        embed = discord.Embed(
+            title="Random Number",
+            description=f"Your random number is..... {round(random.random() * 1000)}",
+        color=discord.Color(int("AF27E4", 16)),
+    )
+        embed.set_footer(text=footer),
+        await message.channel.send(embed=embed)
 
+    elif message.content == f"{PREFIX}ticket-add":
+        author_name = message.author.name
+        user_data = loadHasTicket()
 
-#            COMMMANDS.PY PART
-             # Split the message into the command and arguments
-    parts = message.content.split(" ", 1)
-    command = parts[0].lower()
-    arguments = parts[1] if len(parts) > 1 else ""
-
-    # Get the commands dictionary using the get_commands function from commands.py
-    commands = get_commands(client)
-
-    # Check if the command is recognized
-    if command.startswith(PREFIX) and command[len(PREFIX):] in commands:
-        # Get the response from the commands dictionary
-        response = commands[command[len(PREFIX):]]
-        # If it's an embed, send it as an embed
-        if isinstance(response, discord.Embed):
-            await message.channel.send(embed=response)
+        if user_data["users_with_tickets"].get(author_name):
+            await message.channel.send("You already have a ticket.")
         else:
-            # Otherwise, send it as a normal message
-            await message.channel.send(response)
+            user_data["users_with_tickets"][author_name] = True
+            saveHasTicket(user_data)
+            await message.channel.send("Ticket created.")
+            guild = message.guild
+            await guild.create_text_channel(f"tkc-mpy-{message.author.name}")
+    elif message.content == f"{PREFIX}ticket-rm":
+        author_name = message.author.name
+        user_data = loadHasTicket()
 
+        if user_data["users_with_tickets"].get(author_name):
+            user_data["users_with_tickets"].pop(author_name)
+            saveHasTicket(user_data)
+
+            # Find the channel associated with the user's ticket
+            for channel in message.guild.channels:
+                if channel.name.startswith(f"tkc-mpy-{author_name}"):
+                    await channel.delete()
+                    await message.channel.send("Ticket removed.")
+                    return
+
+            await message.channel.send("Ticket channel not found.")
+        else:
+            await message.channel.send("You don't have a ticket to remove.")
+#           MBGTK ALL COMMANDS DEFINITIONS.
+
+# ALL_COMMANDS = {
+#     "hello",
+#     "ping",
+#     "help",
+#     "random",
+#     "meme",
+#     "settag",
+#     }
 # Run the bot with the provided token
 client.run(TOKEN)
